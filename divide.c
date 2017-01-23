a3a03630391c6848030e48881a32b624a3aa78f0
Herwin Lam <P15206944@GWHXX582WIN014W.lec-admin.dmu.ac.uk>
2017-01-23 13:41:56 +0000

    added divide.c program

---
+++ divide.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+       
+int main()
+{
+    int n1,n2,sum;
+
+    printf("Enters two numbers: ");
+    scanf("%d %d",&n1,&n2);
+
+    sum = n1 / n2;        
+    printf("sum = %d",sum);
+
+    return 0;
+}
+commit;
+
